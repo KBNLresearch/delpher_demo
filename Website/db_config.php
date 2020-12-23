1dcb0aec3f9201305616be77cc575137ab778e08
Mirjam Cuper <mirjam.cuper@gmail.com>
2020-12-23 12:20:33 +0200

    Added new db info

--- Website/db_config.php
+++ Website/db_config.php
@@ -1,5 +1,5 @@
 <?php
 // Inloggen op de database
-$dir = 'sqlite:/var/www/delpher_demo/db/test.db';
+$dir = 'sqlite:/var/www/delpher_demo/db/newspapers_dummy.db';
 $dbh  = new PDO($dir) or die("cannot open the database");
 ?>
